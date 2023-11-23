bbc8ae7ce5d062306602a499984050dd4a394116
Aziz Emre Erdoğan <54072698+azizemreerdogan@users.noreply.github.com>
2023-11-23 20:34:39 +0300

    Add files via upload
    BİTTİ

--- ALGORITHM PROJECT-1.py
+++ ALGORITHM PROJECT-1.py
@@ -1,4 +1,4 @@
-#Constants
+#Constants are declared in Turkish Lira(TL)
 SINGLE_TIME_ENERGY_FEE_INDUSTRIAL = 3.053828
 SINGLE_TIME_ENERGY_FEE_PUBLIC_AND_PRIVATE_SECTOR_LOW = 1.912220
 SINGLE_TIME_ENERGY_FEE_PUBLIC_AND_PRIVATE_SECTOR_HIGH = 2.828414
@@ -14,7 +14,7 @@ DAYTIME_PERIOD_ENERGY_FEE_RESIDENTIAL_LOW = 1.157700
 DAYTIME_PERIOD_ENERGY_FEE_RESIDENTIAL_HIGH = 1.177700
 DAYTIME_PERIOD_ENERGY_FEE_AGRICULTURAL_ACTIVITIES = 1.704822
 PEAK_PERIOD_ENERGY_FEE_INDUSTRIAL = 4.909037
-PEAK_PERIOD_ENERGY_FEE_PUBLIC_AND_PRIVATE_SECTOR_LOW = 1.481941
+PEAK_PERIOD_ENERGY_FEE_PUBLIC_AND_PRIVATE_SECTOR_LOW = 4.588843
 PEAK_PERIOD_ENERGY_FEE_PUBLIC_AND_PRIVATE_SECTOR_HIGH = 4.588843
 PEAK_PERIOD_ENERGY_FEE_RESIDENTIAL_LOW = 2.083645
 PEAK_PERIOD_ENERGY_FEE_RESIDENTIAL_HIGH = 2.083645
@@ -35,13 +35,6 @@ UNIT_DISTRIBUTION_FEE_AGRICULTURAL_ACTIVITIES = 0.721579
 UNIT_DISTRIBUTION_FEE_LIGHTING = 0.841099
 ECT_RATE_INDUSTRIAL = 0.01
 ECT_RATE_NOT_INDUSTRIAL = 0.05
-#ECT_RATE_PUBLIC_AND_PRIVATE_SECTOR_LOW = 0.05
-#ECT_RATE_PUBLIC_AND_PRIVATE_SECTOR_HIGH = 0.05
-#ECT_RATE_RESIDENTIAL_LOW = 0.05
-#ECT_RATE_RESIDENTIAL_HIGH = 0.05
-#ECT_RATE_RESIDENTIAL_FAMILY_MARTYRS = 0.05
-#ECT_RATE_AGRICULTURAL_ACTIVITIES = 0.05
-#ECT_RATE_LIGHTING = 0.05
 VAT_RATE_INDUSTRIAL = 0.2
 VAT_RATE_PUBLIC_AND_PRIVATE = 0.2
 VAT_RATE_RESIDENTIAL = 0.1
@@ -50,10 +43,11 @@ VAT_RATE_LIGHTING = 0.2
 DAILY_AVARAGE_UPPER_LIMIT_PUBLIC_AND_PRIVATE_SECTOR = 30
 DAILY_AVARAGE_UPPER_LIMIT_FOR_RESIDENTIAL= 8
 
+
 #Input validation func
 def validation(x,text):
     number = float(input(f'{text}'))
-    while number < x or type(x) != float:
+    while number < x:
         print('Invalid')
         number = input(f'{text}')
     return number
@@ -66,10 +60,11 @@ def consumption(previous,current):
 #Total fee calculator
 def feeCalc(preferredTariff): #The function should have a parameter beacuse the function is going to be used in calculating profit or loss
     #Two Stage
-    if consumerType == 'Public':
+    if consumerType == 'Public and Private':
         if preferredTariff == 'Single':
             if limitExceed:
-                totalFee = totalConsumption*SINGLE_TIME_ENERGY_FEE_PUBLIC_AND_PRIVATE_SECTOR_HIGH
+                totalFee = DAILY_AVARAGE_UPPER_LIMIT_PUBLIC_AND_PRIVATE_SECTOR*daysBetween*SINGLE_TIME_ENERGY_FEE_PUBLIC_AND_PRIVATE_SECTOR_LOW + limitExceedAmount*SINGLE_TIME_ENERGY_FEE_PUBLIC_AND_PRIVATE_SECTOR_HIGH
+
             else:
                 totalFee = totalConsumption*SINGLE_TIME_ENERGY_FEE_PUBLIC_AND_PRIVATE_SECTOR_LOW
         else:
@@ -82,17 +77,19 @@ def feeCalc(preferredTariff): #The function should have a parameter beacuse the 
     elif consumerType == 'Residential':
         if martyrOrVeteran:
             totalFee = totalConsumption * SINGLE_TIME_ENERGY_FEE_RESIDENTIAL_FAMILY_MARTYRS
-        if preferredTariff == 'Single':
-            if limitExceed:
-                totalFee = totalConsumption*SINGLE_TIME_ENERGY_FEE_RESIDENTIAL_HIGH
-            else:
-                totalFee = totalConsumption*SINGLE_TIME_ENERGY_FEE_RESIDENTIAL_LOW
         else:
-            
-            daytimeFee = consumption(previousDaytime,currentDaytime)*DAYTIME_PERIOD_ENERGY_FEE_RESIDENTIAL_LOW
-            peakFee = consumption(previousPeak,currentPeak)*PEAK_PERIOD_ENERGY_FEE_RESIDENTIAL_LOW
-            nightFee = consumption(previousNight,currentNight)*NIGHT_PERIOD_ENERGY_FEE_RESIDENTIAL_LOW
-            totalFee = daytimeFee + peakFee +  nightFee
+            if preferredTariff == 'Single':
+                if limitExceed:
+                    totalFee = DAILY_AVARAGE_UPPER_LIMIT_FOR_RESIDENTIAL*daysBetween*SINGLE_TIME_ENERGY_FEE_RESIDENTIAL_LOW + limitExceedAmount*SINGLE_TIME_ENERGY_FEE_RESIDENTIAL_HIGH
+                    
+                else:
+                    totalFee = totalConsumption*SINGLE_TIME_ENERGY_FEE_RESIDENTIAL_LOW
+            else:
+                
+                daytimeFee = consumption(previousDaytime,currentDaytime)*DAYTIME_PERIOD_ENERGY_FEE_RESIDENTIAL_LOW
+                peakFee = consumption(previousPeak,currentPeak)*PEAK_PERIOD_ENERGY_FEE_RESIDENTIAL_LOW
+                nightFee = consumption(previousNight,currentNight)*NIGHT_PERIOD_ENERGY_FEE_RESIDENTIAL_LOW
+                totalFee = daytimeFee + peakFee +  nightFee
 
     elif consumerType == 'Industrial':
         if preferredTariff == 'Single':
@@ -124,32 +121,33 @@ def taxCalc(select,totalConsumptionFee):
     if consumerType == 'Industrial':
         unitDistribution = totalConsumption*UNIT_DISTRIBUTION_FEE_INDUSTRIAL
         taxECT = totalConsumptionFee*ECT_RATE_INDUSTRIAL
-        taxVAT = (taxECT + UNIT_DISTRIBUTION_FEE_INDUSTRIAL)*VAT_RATE_INDUSTRIAL
+        taxVAT = ((taxECT+totalConsumptionFee) + unitDistribution)*VAT_RATE_INDUSTRIAL
         
     elif consumerType == 'Public and Private':
         unitDistribution = totalConsumption*UNIT_DISTRIBUTION_FEE_PUBLIC_AND_PRIVATE_SECTOR_HIGH
-        taxECT = totalConsumptionFee*ECT_RATE_NOT_INDUSTRIAL
-        taxVAT = (taxECT + UNIT_DISTRIBUTION_FEE_PUBLIC_AND_PRIVATE_SECTOR_HIGH)
+        taxECT = (totalConsumptionFee)*ECT_RATE_NOT_INDUSTRIAL
+        taxVAT = ((taxECT+totalConsumptionFee) + unitDistribution)*VAT_RATE_PUBLIC_AND_PRIVATE
         
     elif consumerType == 'Residential':
-        taxECT = totalConsumptionFee*ECT_RATE_NOT_INDUSTRIAL
+        taxECT = (totalConsumptionFee)*ECT_RATE_NOT_INDUSTRIAL
         if martyrOrVeteran:
             unitDistribution = totalConsumption*UNIT_DISTRIBUTION_FEE_RESIDENTIAL_FAMILY_MARTYRS
-            taxVAT = (taxECT + UNIT_DISTRIBUTION_FEE_RESIDENTIAL_FAMILY_MARTYRS)*VAT_RATE_RESIDENTIAL
+            taxVAT = ((taxECT+totalConsumptionFee) + unitDistribution)*VAT_RATE_RESIDENTIAL
             
         else:
-            taxVAT = (taxECT+UNIT_DISTRIBUTION_FEE_RESIDENTIAL_HIGH)*VAT_RATE_RESIDENTIAL
-            unitDistribution = totalConsumption*UNIT_DISTRIBUTION_FEE_PUBLIC_AND_PRIVATE_SECTOR_LOW
+            unitDistribution = totalConsumption*UNIT_DISTRIBUTION_FEE_RESIDENTIAL_LOW
+            taxVAT = ((taxECT+totalConsumptionFee) + unitDistribution)*VAT_RATE_RESIDENTIAL
+            
             
     elif consumerType == 'Agricultural':
         unitDistribution = totalConsumption*UNIT_DISTRIBUTION_FEE_AGRICULTURAL_ACTIVITIES
-        taxECT = totalConsumptionFee*ECT_RATE_NOT_INDUSTRIAL
-        taxVAT = (taxECT+UNIT_DISTRIBUTION_FEE_AGRICULTURAL_ACTIVITIES)*VAT_RATE_AGRICULTURAL_ACTIVITIES
+        taxECT = (totalConsumptionFee)*ECT_RATE_NOT_INDUSTRIAL
+        taxVAT = ((taxECT+totalConsumptionFee) + unitDistribution)*VAT_RATE_AGRICULTURAL_ACTIVITIES
         
     else: #Lightning
         unitDistribution = totalConsumption*UNIT_DISTRIBUTION_FEE_LIGHTING
-        taxECT = totalConsumptionFee*ECT_RATE_NOT_INDUSTRIAL
-        taxVAT = (taxECT + UNIT_DISTRIBUTION_FEE_LIGHTING)*VAT_RATE_LIGHTING
+        taxECT = (totalConsumptionFee)*ECT_RATE_NOT_INDUSTRIAL
+        taxVAT = ((taxECT+totalConsumptionFee) + unitDistribution)*VAT_RATE_LIGHTING
         
     
     #The function returns any of these taxes according to a parameter
@@ -175,16 +173,18 @@ def profitLossCalc(preferredTariff):
     newTotalVAT = taxCalc('VAT',newTotalConsumptionFee)
     newUnitDistribution = taxCalc('Distribution',newTotalConsumptionFee)
     newTotalInvoice = newTotalConsumptionFee + newUnitDistribution + newTotalECT + newTotalVAT
-    difference = (newTotalInvoice - newTotalInvoice)
+    difference = (newTotalInvoice - totalInvoice)
 
     if difference > 0:
         profit = True
+
         profitLossAmount = difference
         print(f'The consumer has made a profit at amount of {profitLossAmount}')
 
     else:
         profit = False
-        profitLossAmount = -(difference)
+
+        profitLossAmount = difference
         print(f'The consumer has made a loss at amount of {profitLossAmount}')
     
     return profit
@@ -211,11 +211,19 @@ consumptionCounterResidential = 0
 consumptionCounterPublic = 0
 consumptionCounterAgricultural = 0
 consumptionCounterLightning = 0
+counterMultiLossConsumers = 0
+counterOtherThanMartyrLightning = 0
 while True:
 
-    consumerNo = input('Please enter your consumer no, 0 to exit')
+    
+
+    consumerNo = int(input('Please enter your consumer no, 0 to exit'))
     if consumerNo == 0:
         break
+    
+    martyrOrVeteran = False
+    preferredTariff = 'Single' #Default because when martyr or veteran an error occurs
+    
     consumerType = input('Enter consumer type')
     while consumerType not in['I','i','P','p','R','r','A','a','L','l']:
         print()
@@ -248,7 +256,7 @@ while True:
             martyrOrVeteran = False
 
 
-    if (not martyrOrVeteran) and (consumerType != 'L' or consumerType != 'l'):
+    if (not martyrOrVeteran) and (consumerType != 'Lightning'):
         preferredTariff = input('Enter preferred tariff')
         while preferredTariff not in ['S','s','M','m']:
             print()
@@ -261,23 +269,26 @@ while True:
 
 
 
-    previousDaytime = validation(0,'Please enter previous daytime')
-    currentDaytime = validation(previousDaytime,'Please enter current daytime')
-    previousPeak = validation(0,'Enter previous peak')
-    currentPeak = validation(previousPeak,'Please enter current')
-    previousNight = validation(0,'Please enter previous night')
-    currentNight = validation(previousPeak,'Please enter current night')
+    previousDaytime = validation(0,'Please enter previous daytime consumption')
+    currentDaytime = validation(previousDaytime,'Please enter current daytime consumption')
+    previousPeak = validation(0,'Enter previous Peak-time consumption')
+    currentPeak = validation(previousPeak,'Please enter current Peak-time consumption')
+    previousNight = validation(0,'Please enter previous Night-time consumption')
+    currentNight = validation(previousNight,'Please enter current Night-time consumption')
 
 
     daysBetween = validation(1,'Please enter days between')
-    totalConsumptionOfYear = validation(0,'Please enter total consumption made')
+    totalConsumptionOfYear = validation(0,'Please enter total consumption made in the year')
     totalConsumption = consumption(previousDaytime,currentDaytime) + consumption(previousPeak,currentPeak) + consumption(previousNight,currentNight)
 
+    dailyAverage = totalConsumption/daysBetween
+
     if consumerType == 'Public and Private':
-        if totalConsumption / daysBetween <= 30:
+        if dailyAverage <= 30: 
             limitExceed = False
         else:
             limitExceed = True
+            limitExceedAmount = (dailyAverage-30)*daysBetween
         if preferredTariff == 'Single':
             publicSingle += 1
     elif consumerType == 'Residential':
@@ -285,6 +296,7 @@ while True:
             limitExceed = False
         else:
             limitExceed = True
+            limitExceedAmount = (dailyAverage-8)*daysBetween
 
 
 
@@ -315,19 +327,19 @@ while True:
     match consumerType:
         case 'Industrial':
             counterIndustrial+=1
-            consumptionCounterIndustrial += 1
+            consumptionCounterIndustrial += totalConsumption
         case 'Residential':
             counterResidential+=1
-            consumptionCounterResidential +=1
+            consumptionCounterResidential += totalConsumption
         case 'Public and Private':
             counterPublic+=1
-            consumptionCounterPublic += 1
+            consumptionCounterPublic += totalConsumption
         case 'Agricultural':
             counterAgricultural+=1
-            consumptionCounterAgricultural+=1
+            consumptionCounterAgricultural+= totalConsumption
         case 'Lightning':
             counterLightning+=1
-            consumptionCounterLightning+=1
+            consumptionCounterLightning+= totalConsumption
 
            
         
@@ -339,7 +351,7 @@ while True:
     if consumerType == 'Public and Private':
         publicDailyAverageCounter += dailyAverage
     
-    allCustomersGDZRevenue += totalConsumptionFee
+    allCustomersGDZRevenue += totalConsumptionFee + taxCalc('Distrubution',totalConsumptionFee)
     allCustomersECTRevenue += totalECT
     allCustomersVATRevenue  += totalVAT
 
@@ -351,19 +363,26 @@ while True:
         
     #Prints for each consumer
 
-    print(consumerNo)
-    print(consumerType)
-    print(consumption(previousDaytime,currentDaytime))
-    print(consumption(previousPeak,currentPeak))
-    print(consumption(previousNight,currentNight))
-    print(totalConsumption)
-    print(round(totalConsumptionFee,2))
-    print(round(totalECT,2))
-    print(round(totalVAT,2))
-    print(round(totalInvoice,2))
+    print(f'Consumers number: {consumerNo}')
+    print(f'Consumers type: {consumerType}')
+    print(f'Consumers Daytime consumption: {consumption(previousDaytime,currentDaytime)}')
+    print(f'Consumers Peak-time consumption: {consumption(previousPeak,currentPeak)}')
+    print(f'Consumers Night-time consumption: {consumption(previousNight,currentNight)}')
+    print(f'Consumers total periodic consumption: {totalConsumption}')
+    print(f'Consumers total consumption fee: {round(totalConsumptionFee,2)}')
+    print(f'Total ECT amount: {round(totalECT,2)}')
+    print(f'Total VAT amount: {round(totalVAT,2)}')
+    print(f'Total Invoice: {round(totalInvoice,2)}')
 
+    
     if not(martyrOrVeteran) and consumerType != 'Lightning':
-        profitLossCalc(preferredTariff)
+        profit = profitLossCalc(preferredTariff)
+        
+        counterOtherThanMartyrLightning += 1
+        if preferredTariff == 'Multi' and not(profit):
+            counterMultiLossConsumers += 1
+
+
 
 
     print(totalConsumptionOfYear)
@@ -374,7 +393,7 @@ while True:
     else:
         freeConsumer = True
         print('The consumer deserves to be a free consumer')
-    if consumerType == 'Industrial' and (freeConsumer or totalInvoice > 100000): #Should do a TL conversion
+    if consumerType == 'Industrial' and (totalConsumptionOfYear> 10000 or totalInvoice > 100000): #Should do a TL conversion
         overusageIndustry += 1
 
 
@@ -382,6 +401,7 @@ while True:
 totalConsumers = counterLightning + counterAgricultural + counterIndustrial + counterPublic + counterResidential
 publicMulti = counterPublic - publicSingle
 publicDailyAverage = publicDailyAverageCounter / counterPublic
+
 
 
 #printing all of the information
@@ -418,7 +438,26 @@ print(f'Total electricty consumption for Industrial {consumptionCounterLightning
 
 print(f'Bornova\s total consumption is {allCustomersConsumption}')
 
-print(f'Number of public, private and other {}')
+print(f'Number of public, private and other who prefer single{publicSingle}')
+print(f'Number of public, private and other who prefer multi{publicMulti}')
+print(f'Number of public, private and other single percentage{publicSingle/counterPublic*100}')
+
+print(f'Number of Industry consumer who is over 10000kWh or 100000TL and their percentage: {overusageIndustry}, {overusageIndustry/counterIndustrial*100}')
+print(f'Consumer no of the residential type with the highest daily average consumption: {maxResidentialDailyAverageNo}')
+print(f'This consumers daily average electricity usage: {maxResidentialDailyAverage}')
+print(f'This consumers total bill: {maxTotalInvoiceDailyAverage}')
+
+print(f'Consumer no of highest total bill other than residential: {maxTotalInvoiceNo}')
+print(f'Consumer type of this consumer: {maxTotalInvoiceType}')
+print(f'Daily average electricity consumption of this consumer: {maxTotalInvoiceDailyAverage}')
+print(f'Total bill amount of this consumer: {maxTotalInvoice}')
+
+print(f'Total revenue earned by GDZ{allCustomersGDZRevenue}')
+print(f'Total revenue of municipality {allCustomersECTRevenue}')
+print(f'Total revenue of the state {allCustomersVATRevenue}')
+
+print(f'The precentage of those who made a loss {counterMultiLossConsumers/counterOtherThanMartyrLightning*100}')
+
 
 
 
